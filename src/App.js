4acd15da28129deed366f033d79b1e83e747295e
samhithabasireddy <reddysamhi11@gmail.com>
2025-10-04 15:03:09 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import react from 'react';
+
+function App() {
+  return (
+   
+        <div>
+            <div>
+              <center><h1>My Portfolio!</h1>
+              <h1>Samhitha Basireddy</h1></center>
+            </div>
+            <div>
+               <h4>7842511439</h4>
+               <h4>reddysamhi11@gmail.com</h4>
+            </div>
+            <div>
+               <h2>SKILLS</h2>
+               <h4> Programming Languages: C,Python,HTML</h4>
+               <h2> EDUCATION</h2>
+               <h4> Bachelor of Technology in In Technology- Cgpa:8.3 till now
+Intermediate from The Little Flower Junior College with percentage:96 </h4>
+               <h2>PROJECTS</h2>
+               <h3> Web Development of a Makeup Website using HTML,CSS,JSP</h3>
+               <h4>•The website is designed in the user-friendly manner.
+•It consists of a Sign-in page and a log-in page where the users can shop after providing the required information for
+registration.
+•Later the can shop the products available.</h4>
+               <h3>Web Development of a Online Coding Platform using HTML,CSS,Javascript</h3>
+               <h4>•This platform is for the school learning students to start their interest in coding.
+•The basic information about the programming languages with the video of it provided.
+•They can attempt to the quiz for more practice.
+               </h4>
+               <h2>CERTIFICATES</h2>
+               <h4> 
+•Certificate for Blind Coding in JIGNASA Fest.
+•Certificate for R programming.
+•Certificate for Naurkri Campus Young Minds.
+               </h4>
+            </div>
+        </div>
+
+  );
+}
+
+export default App;
